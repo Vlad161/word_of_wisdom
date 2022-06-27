74befd46f4242c1a7edb653202fe5bf92eeda584
Vladislav Bagdasaryan <bagdasaryanvlad@gmail.com>
2022-06-28 00:19:52 +0300

    Added token storage element lifetime

--- server/token/storage.go
+++ server/token/storage.go
@@ -1,19 +1,27 @@
 package token
 
 import (
+	"context"
 	"sync"
+	"time"
 )
 
-// Single use tokens storage. After successful verify token delete from storage.
 type storage struct {
 	sync.RWMutex
-	tokens map[string]struct{} //TODO tokens are infinitive. Create job to clear old values
+	tokens    map[string]struct{}
+	oldTokens map[string]struct{}
 }
 
-func NewStorage() *storage {
-	return &storage{
-		tokens: make(map[string]struct{}),
+// NewStorage is a single use tokens storage. After successful verify token delete from storage.
+// Token guaranteed storage at least tokenLifeTime duration.
+func NewStorage(ctx context.Context, tokenLifeTime time.Duration) *storage {
+	s := &storage{
+		tokens:    make(map[string]struct{}),
+		oldTokens: make(map[string]struct{}),
 	}
+
+	s.cleanupWorker(ctx, tokenLifeTime)
+	return s
 }
 
 func (s *storage) Put(v string) {
@@ -24,14 +32,39 @@ func (s *storage) Put(v string) {
 
 func (s *storage) Verify(v string) bool {
 	s.RLock()
-	if _, ok := s.tokens[v]; ok {
+	if isContain(s.tokens, v) || isContain(s.oldTokens, v) {
 		s.RUnlock()
 		s.Lock()
 		delete(s.tokens, v)
+		delete(s.oldTokens, v)
 		s.Unlock()
 		return true
 	}
 
 	s.RUnlock()
 	return false
+}
+
+func isContain(m map[string]struct{}, key string) bool {
+	_, ok := m[key]
+	return ok
+}
+
+func (s *storage) cleanupWorker(ctx context.Context, tokenLifeTime time.Duration) {
+	go func() {
+		ticker := time.NewTicker(tokenLifeTime)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				s.Lock()
+				s.oldTokens = s.tokens
+				s.tokens = make(map[string]struct{})
+				s.Unlock()
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 }
--- server/token/storage_test.go
+++ server/token/storage_test.go
@@ -1,36 +1,56 @@
 package token_test
 
 import (
+	"context"
 	"math/rand"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"word_of_wisdom/server/token"
 )
 
 func TestStorage(t *testing.T) {
-	var (
-		gCount  = 10
-		maxJ    = 100
-		r       = rand.New(rand.NewSource(time.Now().Unix()))
-		storage = token.NewStorage()
-		wg      = sync.WaitGroup{}
-	)
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
 
-	wg.Add(gCount)
-	for i := 0; i < gCount; i++ {
-		go func() {
-			for j := 0; j < r.Intn(maxJ); j++ {
-				ts := token.New()
-				storage.Put(ts)
-				assert.True(t, storage.Verify(ts))
-				assert.False(t, storage.Verify(ts))
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
+	t.Run("multi goroutine read/write", func(t *testing.T) {
+		var (
+			gCount  = 10
+			maxJ    = 100
+			r       = rand.New(rand.NewSource(time.Now().Unix()))
+			storage = token.NewStorage(ctx, 1*time.Second)
+			wg      = sync.WaitGroup{}
+		)
+
+		wg.Add(gCount)
+		for i := 0; i < gCount; i++ {
+			go func() {
+				for j := 0; j < r.Intn(maxJ); j++ {
+					ts := token.New()
+					storage.Put(ts)
+					assert.True(t, storage.Verify(ts))
+					assert.False(t, storage.Verify(ts))
+				}
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
+
+	t.Run("token lifetime", func(t *testing.T) {
+		var (
+			tokenLifeTime = 50 * time.Millisecond
+			storage       = token.NewStorage(ctx, tokenLifeTime)
+		)
+
+		ts := token.New()
+		storage.Put(ts)
+		time.Sleep(tokenLifeTime*2 + 1)
+
+		require.False(t, storage.Verify(ts))
+	})
 }
